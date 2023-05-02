f30a21184b60b39dc25aaf3cd37793ac73f313f7
Abhishek <abhisahu1908@gmail.com>
2023-05-02 09:45:07 +0530

    adding meal section

---
+++ src/components/Meals/MealSummary.jsx
@@ -0,0 +1,18 @@
+
+const MealSummary =()=>{
+   return(
+    <section className="max-w-[40rem] w-[90%] text-center m-auto mt-[-10rem] relative text-white bg-[#383838] rounded-xl p-4  shadow-[0_1px_18px_10px_rgba(0,0,0,0.25)]">
+        <h1 className="text-3xl font-bold mb-5">Delicious Food , Delivered To You !</h1>
+        <p> 
+        Choose your favorite meal from our broad selection of available meals
+        and enjoy a delicious lunch or dinner at home.
+       </p>
+       <p>
+        All our meals are cooked with high-quality ingredients, just-in-time and
+        of course by experienced chefs!
+      </p>
+    </section>
+   )
+}
+
+export default MealSummary;
